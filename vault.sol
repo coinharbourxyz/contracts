354f585e43fa91124830d1242c055bb36c080591
Shubham Palriwala <spalriwalau@gmail.com>
2024-12-12 20:59:23 +0530

    feat: create bucket with custom tokens & weights

--- vault.sol
+++ vault.sol
@@ -6,28 +6,43 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract VaultToken is ERC20, Ownable {
-    AggregatorV3Interface private wbtcPriceFeed;
-    AggregatorV3Interface private ethPriceFeed;
+    struct TokenData {
+        address tokenAddress;
+        AggregatorV3Interface priceFeed;
+        uint256 weight;
+    }
 
-    uint256 public wbtcWeight = 50; // 50% weight
-    uint256 public ethWeight = 50;  // 50% weight
-
-    address public wbtc;
-    address public eth;
-
+    TokenData[] public tokens;
     uint256 private constant PRECISION = 1e8;
+    address public usdtAddress = 0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0; // sepolia
 
     constructor(
         string memory name,
         string memory symbol,
-        address initialOwner
+        address initialOwner,
+        address[] memory tokenAddresses,
+        address[] memory priceFeeds,
+        uint256[] memory weights
     ) ERC20(name, symbol) Ownable(initialOwner) {
-        wbtcPriceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43); // sepolia
-        ethPriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); // sepolia
-        wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
-        eth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    }
+        require(
+            tokenAddresses.length == priceFeeds.length &&
+            priceFeeds.length == weights.length,
+            "Arrays must be of equal length"
+        );
 
+        uint256 totalWeight = 0;
+        for (uint256 i = 0; i < weights.length; i++) {
+            require(weights[i] > 0, "Weight must be positive");
+            tokens.push(TokenData({
+                tokenAddress: tokenAddresses[i],
+                priceFeed: AggregatorV3Interface(priceFeeds[i]),
+                weight: weights[i]
+            }));
+            totalWeight += weights[i];
+        }
+
+        require(totalWeight == 100, "Total weights must sum to 100");
+    }
 
     function getLatestPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         (, int256 price, , , ) = priceFeed.latestRoundData();
@@ -36,19 +51,20 @@ contract VaultToken is ERC20, Ownable {
     }
 
     function calculateVaultTokenValue() public view returns (uint256) {
-        uint256 wbtcPrice = getLatestPrice(wbtcPriceFeed);
-        uint256 ethPrice = getLatestPrice(ethPriceFeed);
+        uint256 totalValue = 0;
 
-        uint256 wbtcValue = (wbtcPrice * wbtcWeight) / (100 * PRECISION);
-        uint256 ethValue = (ethPrice * ethWeight) / (100 * PRECISION);
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uint256 price = getLatestPrice(tokens[i].priceFeed);
+            // uint256 balance = ERC20(tokens[i].tokenAddress).balanceOf(address(this));
+            uint256 value = (price * tokens[i].weight) / (100 * PRECISION);
+            totalValue += value;
+        }
 
-        return wbtcValue + ethValue;
+        return totalValue;
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
-        uint256 vaultValue = calculateVaultTokenValue();
-        require(vaultValue >= amount, "Insufficient collateral");
-
+        require(calculateVaultTokenValue() >= amount, "Insufficient collateral");
         _mint(to, amount);
     }
 
@@ -56,19 +72,25 @@ contract VaultToken is ERC20, Ownable {
         _burn(from, amount);
     }
 
-    function updateWeights(uint256 newWbtcWeight, uint256 newEthWeight) external onlyOwner {
-        require(newWbtcWeight + newEthWeight == 100, "Weights must sum to 100");
-        wbtcWeight = newWbtcWeight;
-        ethWeight = newEthWeight;
+    function deposit(uint256 amount) external {
+        uint256 vaultTokenValue = calculateVaultTokenValue();
+        require(vaultTokenValue > 0, "Vault value must be greater than zero");
+
+        require(ERC20(usdtAddress).transferFrom(msg.sender, address(this), amount), "USDC transfer failed");
+        uint256 tokensToMint = (amount * 1e8) / vaultTokenValue; // 1e8 is used for precision adjustment
+        _mint(msg.sender, tokensToMint);
     }
 
-    function deposit(address token, uint256 amount) external {
-        require(token == wbtc || token == eth, "Unsupported token");
-        ERC20(token).transferFrom(msg.sender, address(this), amount);
+    function withdraw(uint256 amount) external {
+        uint256 vaultTokenValue = calculateVaultTokenValue();
+        require(vaultTokenValue > 0, "Vault value must be greater than zero");
+
+        // Burn the VaultTokens from the user
+        _burn(msg.sender, amount);
+
+        // Transfer the corresponding USDC to the user
+        uint256 usdcToTransfer = (amount * vaultTokenValue) / 1e18;
+        require(ERC20(usdtAddress).transfer(msg.sender, usdcToTransfer), "USDC transfer failed");
     }
 
-    function withdraw(address token, uint256 amount) external onlyOwner {
-        require(token == wbtc || token == eth, "Unsupported token");
-        ERC20(token).transfer(msg.sender, amount);
-    }
 }
