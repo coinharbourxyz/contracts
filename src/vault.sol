be0ad5f7ba63bc3bdf767af70d06ac7d104b94b9
Aashish Paliwal <aashish.paliwal@live.com>
2024-12-19 21:41:10 +0530

    integrated uniswap into vault to buy vault token

--- src/vault.sol
+++ src/vault.sol
@@ -3,7 +3,20 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+interface IUniswapV3 {
+    function swapExactInputSingleHop(
+        address tokenIn,
+        address tokenOut,
+        uint24 poolFee,
+        uint256 amountIn
+    ) external returns (uint256 amountOut);
+
+    function wrapETH() external payable;
+
+    function unwrapETH(uint256 amount) external;
+}
 
 contract VaultToken is ERC20, Ownable {
     struct TokenData {
@@ -12,9 +25,15 @@ contract VaultToken is ERC20, Ownable {
         uint256 weight;
     }
 
+    address public uniswapContract;
     TokenData[] public tokens;
     uint256 private constant PRECISION = 1e8;
     address public usdtAddress = 0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0; // sepolia
+    // mapping(address => uint256) public allocations;
+    mapping(address => uint256) public tokenBalances;
+
+    event AllocationsUpdated(address[] tokens, uint256[] weights);
+    event TokensSwapped(address tokenOut, uint256 amountReceived);
 
     constructor(
         string memory name,
@@ -22,29 +41,35 @@ contract VaultToken is ERC20, Ownable {
         address initialOwner,
         address[] memory tokenAddresses,
         address[] memory priceFeeds,
-        uint256[] memory weights
+        uint256[] memory weights,
+        address _uniswapContract
     ) ERC20(name, symbol) Ownable(initialOwner) {
         require(
             tokenAddresses.length == priceFeeds.length &&
-            priceFeeds.length == weights.length,
+                priceFeeds.length == weights.length,
             "Arrays must be of equal length"
         );
 
         uint256 totalWeight = 0;
         for (uint256 i = 0; i < weights.length; i++) {
             require(weights[i] > 0, "Weight must be positive");
-            tokens.push(TokenData({
-                tokenAddress: tokenAddresses[i],
-                priceFeed: AggregatorV3Interface(priceFeeds[i]),
-                weight: weights[i]
-            }));
+            tokens.push(
+                TokenData({
+                    tokenAddress: tokenAddresses[i],
+                    priceFeed: AggregatorV3Interface(priceFeeds[i]),
+                    weight: weights[i]
+                })
+            );
             totalWeight += weights[i];
         }
 
         require(totalWeight == 100, "Total weights must sum to 100");
+        uniswapContract = _uniswapContract;
     }
 
-    function getLatestPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    function getLatestPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
         (, int256 price, , , ) = priceFeed.latestRoundData();
         require(price > 0, "Invalid price");
         return uint256(price);
@@ -70,7 +95,7 @@ contract VaultToken is ERC20, Ownable {
     ) external onlyOwner {
         require(
             tokenAddresses.length == priceFeeds.length &&
-            priceFeeds.length == weights.length,
+                priceFeeds.length == weights.length,
             "Arrays must be of equal length"
         );
 
@@ -79,7 +104,7 @@ contract VaultToken is ERC20, Ownable {
             require(weights[i] > 0, "Weight must be positive");
             totalWeight += weights[i];
         }
-        
+
         require(totalWeight == 100, "Total weights must sum to 100");
 
         // Clear the current tokens array
@@ -87,17 +112,22 @@ contract VaultToken is ERC20, Ownable {
 
         // Add the new tokens and their weights
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
-            tokens.push(TokenData({
-                tokenAddress: tokenAddresses[i],
-                priceFeed: AggregatorV3Interface(priceFeeds[i]),
-                weight: weights[i]
-            }));
+            tokens.push(
+                TokenData({
+                    tokenAddress: tokenAddresses[i],
+                    priceFeed: AggregatorV3Interface(priceFeeds[i]),
+                    weight: weights[i]
+                })
+            );
         }
+        emit AllocationsUpdated(tokenAddresses, weights);
     }
 
-
     function mint(address to, uint256 amount) external onlyOwner {
-        require(calculateVaultTokenValue() >= amount, "Insufficient collateral");
+        require(
+            calculateVaultTokenValue() >= amount,
+            "Insufficient collateral"
+        );
         _mint(to, amount);
     }
 
@@ -105,11 +135,67 @@ contract VaultToken is ERC20, Ownable {
         _burn(from, amount);
     }
 
-    function deposit(uint256 amount) external {
+    function buyTokens(
+        address tokenIn,
+        uint24 poolFee,
+        uint256 totalAmount
+    ) external payable {
+        require(totalAmount > 0, "Invalid amount");
+        require(
+            msg.value == totalAmount,
+            "ETH sent does not match totalAmount"
+        );
+
+        // Wrap ETH into WETH
+        IUniswapV3(uniswapContract).wrapETH{value: totalAmount}();
+
+        uint256 totalMintedValue = 0;
+
+        // Distribute the totalAmount according to weights in tokens array
+        for (uint256 i = 0; i < tokens.length; i++) {
+            address tokenOut = tokens[i].tokenAddress;
+            uint256 allocationWeight = tokens[i].weight;
+            uint256 amountIn = (totalAmount * allocationWeight) / 100;
+
+            // Handle precision for token swaps
+            uint256 preciseAmountIn = (amountIn * PRECISION) / 100;
+
+            // Perform the swap using UniswapV3
+            uint256 amountOut = IUniswapV3(uniswapContract)
+                .swapExactInputSingleHop(
+                    tokenIn,
+                    tokenOut,
+                    poolFee,
+                    preciseAmountIn
+                );
+
+            // Update token balance in the vault
+            tokenBalances[tokenOut] += amountOut;
+
+            // Accumulate the minted value based on the price of the token
+            uint256 tokenPrice = getLatestPrice(tokens[i].priceFeed);
+            totalMintedValue += (amountOut * tokenPrice) / PRECISION;
+
+            emit TokensSwapped(tokenOut, amountOut);
+        }
+
+        // Issue fund tokens proportional to the deposited value
+        _mint(msg.sender, totalMintedValue / calculateVaultTokenValue());
+    }
+
+    function deposit(address tokenIn, uint256 amount) external {
         uint256 vaultTokenValue = calculateVaultTokenValue();
         require(vaultTokenValue > 0, "Vault value must be greater than zero");
 
-        require(ERC20(usdtAddress).transferFrom(msg.sender, address(this), amount), "USDC transfer failed");
+        // Transfer tokens from the user to this contract
+        require(
+            ERC20(tokenIn).transferFrom(msg.sender, address(this), amount),
+            "Token transfer failed"
+        );
+
+        // Approve the Uniswap contract to spend the tokens
+        ERC20(tokenIn).approve(uniswapContract, amount);
+
         uint256 tokensToMint = (amount * 1e8) / vaultTokenValue; // 1e8 is used for precision adjustment
         _mint(msg.sender, tokensToMint);
     }
@@ -123,7 +209,9 @@ contract VaultToken is ERC20, Ownable {
 
         // Transfer the corresponding USDC to the user
         uint256 usdcToTransfer = (amount * vaultTokenValue) / 1e18;
-        require(ERC20(usdtAddress).transfer(msg.sender, usdcToTransfer), "USDC transfer failed");
+        require(
+            ERC20(usdtAddress).transfer(msg.sender, usdcToTransfer),
+            "USDC transfer failed"
+        );
     }
-
 }
